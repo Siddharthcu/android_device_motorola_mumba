04e86c4f0361ad649417f95bf11e1b362a85b93c
Alexander Koskovich <akoskovich@pm.me>
2022-05-24 06:16:11 -0700

    mumba: Import GPS configuration from WWAA36V.48-12-ST12.1
    Change-Id: I64c78ddc6af425117af8dd85f2fc9a49ac3bb942

--- device.mk
+++ device.mk
@@ -178,6 +178,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf
+
 # Graphics
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml \
